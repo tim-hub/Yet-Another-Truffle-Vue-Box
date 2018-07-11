ca2d9a4cac0f6293e260295394a5abd2ae51dff6
tim-hub <tim_w@outlook.com>
2018-07-11 16:43:31 +1200

    use ganache gui

--- truffle.js
+++ truffle.js
@@ -5,9 +5,9 @@ module.exports = {
   networks: {
     development: {
       host: '127.0.0.1',
-      port: 8545,
+      port: 7545,
       network_id: '*', // Match any network id
-      gas: 470000
+
     }
   }
 }
